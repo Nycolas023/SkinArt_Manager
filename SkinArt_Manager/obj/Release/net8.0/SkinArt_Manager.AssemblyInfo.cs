cdc7f3c8ae79a089f8f12462e6ecc6546ca4ecd6
hubisu <luizhenriquepretodasilva@gmail.com>
2025-06-25 22:25:40 -0300

    gitignore do projeto

--- SkinArt_Manager/obj/Release/net8.0/SkinArt_Manager.AssemblyInfo.cs
+++ SkinArt_Manager/obj/Release/net8.0/SkinArt_Manager.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkinArt_Manager")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+317d258e52e921c7299192313a25bac6155bc2d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkinArt_Manager")]
